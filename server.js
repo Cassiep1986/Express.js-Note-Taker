b645d37bfa6723e1b36d54019ca584ed5c17c9d0
seacrest <cassiep1986@gmail.com>
2021-07-01 01:29:17 -0700

    testing port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const uuid = require("uuid");
 const app = express();
 
-const PORT = 8080;
+// const PORT = 8080;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
